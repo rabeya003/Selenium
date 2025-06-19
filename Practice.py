2f3a2c69391553bc84c53b822dc6be6e730249d9
rabeya003 <rabeya003@gmail.com>
2025-06-19 13:52:37 +0600

    Add login functionality with username, password, and submit button

--- Practice.py
+++ Practice.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
 # 1. Open Chrome
@@ -11,11 +12,17 @@ driver.maximize_window()
 # 2. Navigate to the OrangeHRM login page
 driver.get("https://opensource-demo.orangehrmlive.com/")
 
+
 # 3. Wait for the login form to be present
 WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "txtUsername"))
+        EC.presence_of_element_located((By.NAME, "username"))
     )   
 
+#4. Enter username and password
+driver.find_element(By.NAME, "username").send_keys("Admin")
+time.sleep(2) 
+driver.find_element(By.NAME, "password").send_keys("admin123")
+driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
 
-time.sleep(3)
+time.sleep(5)
 driver.quit()
