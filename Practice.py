5d5fa800450cb068122c7cd2e57b93416cff93be
rabeya003 <rabeya003@gmail.com>
2025-06-19 12:51:27 +0600

    Add explicit wait for login form

--- Practice.py
+++ Practice.py
@@ -5,7 +5,17 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 # 1. Open Chrome
-driver = webdriver.Firefox ()
-driver.get("https://www.google.com")
+driver = webdriver.Chrome ()
+driver.maximize_window()
+
+# 2. Navigate to the OrangeHRM login page
+driver.get("https://opensource-demo.orangehrmlive.com/")
+
+# 3. Wait for the login form to be present
+WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "txtUsername"))
+    )   
+
+
 time.sleep(3)
 driver.quit()
