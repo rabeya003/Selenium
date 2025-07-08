ab9c7642a5c622458b88c3d276e8b132fb01215e
rabeya003 <rabeya003@gmail.com>
2025-07-08 15:04:18 +0600

    Add element visibility and enablement checks for search box

---
+++ ConditionalCommand.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# 1. Open Chrome
+driver = webdriver.Chrome ()
+driver.maximize_window()
+# 2. Go to login page
+driver.get("https://demo.nopcommerce.com/register")
+
+#is_displayed() - to check if an element is visible on the page
+# is_enabled() - to check if an element is enabled (clickable)  
+searchhBox=driver.find_element(By.XPATH,"//input[@id='small-searchterms']")
+print("Display status:",searchhBox.is_displayed())
+print("Enabled status:",searchhBox.is_enabled())
+
+
+
+# 6. Close browser after 20 seconds
+time.sleep(20)
+driver.quit()
